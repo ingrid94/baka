183264a91d5b9ebe00166a359f3e56a0c93c900f
ingrid94 <ingrid.kaasik@eesti.ee>
2016-08-11 11:11:56 +0300

    Finds right place to put the inside block. The line is weird and need to add moving right place when mouse released

--- main.py
+++ main.py
@@ -22,6 +22,13 @@ class Block:
         for el in self.default_items_id:
             if el in closest_objects:
                 closest_objects.remove(el)
+        # Blocks can't connect with InsideBlocks
+        temp = []
+        for elem in closest_objects:
+            if isinstance(movable_blocks[elem], InsideBlock):
+                temp.append(elem)
+        for elem in temp:
+            closest_objects.remove(elem)
         # if there is "line" in dictionary then deletes it
         if "line" in movable_blocks.values():
             self.line_delete(movable_blocks)
@@ -150,7 +157,7 @@ class ControlBlockLower(Block):
 
     def renew_magnets(self):
         upper_magnet = [self.coords[0] + 40, self.coords[1] + 5]
-        lower_magnet = [self.coords[0] + 40, self.coords[1] + 25]
+        lower_magnet = [self.coords[0] + 35, self.coords[1] + 25]
         return [upper_magnet, lower_magnet]
 
     def change_coords(self, delta_x, delta_y):
@@ -319,6 +326,7 @@ class PrintBlock(CommandBlock):
 class InsideBlock:
     def __init__(self, coords, canvas, poly_cords, color, outline):
         # inside the block
+        self.default_items_id = []
         self.coords = coords
         self.canvas = canvas
         self.poly_cords = poly_cords
@@ -333,7 +341,7 @@ class InsideBlock:
         self.obj_id = self.canvas.create_polygon(self.poly_cords, fill=self.color, outline=self.outline)
         return self.obj_id
 
-    def renew_magnet(self):
+    def renew_magnets(self):
         magnet = [self.coords[0], self.coords[1] + 5]
         return magnet
 
@@ -341,12 +349,52 @@ class InsideBlock:
         old_coords = self.coords
         self.coords = [old_coords[0] + delta_x, old_coords[1] + delta_y]
         self.canvas.move(self.obj_id, delta_x, delta_y)
-        self.renew_magnet()
+        self.renew_magnets()
+
+    def get_closest(self, movable_blocks):
+        magnet_x = movable_blocks[self.obj_id].renew_magnets()[0]
+        magnet_y = movable_blocks[self.obj_id].renew_magnets()[1]
+        # finds closest object to magnet
+        closest_object = self.canvas.find_overlapping(magnet_x-5, magnet_y-5, magnet_x+5, magnet_y+5)
+        closest_objects = list(closest_object)
+        closest_objects.remove(self.obj_id)
+        # since TypeBlock is only one where there can't be this connection
+        for el in closest_objects:
+            if isinstance(movable_blocks[el], TypeBlock):
+                closest_objects.remove(el)
+        for el in self.default_items_id:
+            if el in closest_objects:
+                closest_objects.remove(el)
+        # if there is "line" in dictionary then deletes it
+        if "line" in movable_blocks.values():
+            self.line_delete(movable_blocks)
+        return closest_objects
+
+    def check_magnets_during_move(self, movable_blocks):
+        # moves magnets also checks other blocks and marks them
+        closest_object = self.get_closest(movable_blocks)
+        if closest_object != [] and movable_blocks[closest_object[0]] != "line":
+            stable_instance = movable_blocks[closest_object[0]]
+            # gets poly_coords
+            stable_coords = stable_instance.inside_poly_coords
+            # draws "line" mark to the block
+            line_id = self.canvas.create_line(stable_coords, fill="green", width=3)
+            movable_blocks[line_id] = "line"
+
+    # deletes "line" mark
+    def line_delete(self, movable_blocks):
+        line_key = None
+        for key in movable_blocks:
+            if movable_blocks[key] == "line":
+                self.canvas.delete(key)
+                line_key = key
+        if line_key is not None:
+            del movable_blocks[line_key]
 
     def move_to_magnet(self, movable_blocks):
         # when mouse press is let go, puts the block in right place
-        magnet_x = self.renew_magnet()[0]
-        magnet_y = self.renew_magnet()[1]
+        magnet_x = self.renew_magnets()[0]
+        magnet_y = self.renew_magnets()[1]
 
     #    closest_object = self.get_closest(movable_blocks)
     #    if closest_object[0] != self.obj_id:
@@ -387,6 +435,7 @@ class TypeBlock(InsideBlock):
 
     def create_text(self):
         self.text_id = self.canvas.create_text(self.text_coords, anchor=NW, text=self.string_on_block)
+        self.default_items_id.append(self.text_id)
         return self.text_id
 
     def move_connected(self, delta_x, delta_y):
@@ -543,6 +592,10 @@ class ChooseBlocksCanvas:
                                    tags='return_block')
         self.canvas.create_text(60, 70, anchor=NW, text='return', tags='return_block')
         self.canvas.create_polygon(self.inside_block_coords(105, 70, 40, 15), fill='light pink', tags='return_block')
+
+        #self.canvas.create_polygon(self.command_block_coords(50, 100, 30, 120), fill='violet red', outline='purple',
+        #                           tags='assign_block')
+
 
         self.canvas.create_polygon(self.inside_block_coords(50, 110, 130, 20), fill='dodger blue', outline='steel blue',
                                    tags='inside_block')

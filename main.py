8ba511529b67e23586090baeaaf98094e282a9b4
Ingrid <ingrid.kaasik@eesti.ee>
2017-01-01 13:19:03 +0200

    windows are looking good

--- main.py
+++ main.py
@@ -7,18 +7,17 @@ from MoveBlocksCanvas import MoveBlocksCanvas
 
 def main():
     root = Tk()
-    #root2 = Tk()
     root.geometry("+1+1")
     Button(command=root.quit, text="Quit").pack()
     myFont = Font(family="Verdana", size=10)
     myFontBold = Font(family="Verdana", size=10, weight='bold')
     t1 = ChooseBlocksCanvas(root, myFont, myFontBold)
-    t1.top.geometry("500x500")
+    t1.top.geometry("+10+120")
     ChooseBlocksCanvas.create_blocks_fst(t1)
     t2 = MoveBlocksCanvas(root, t1, myFont, myFontBold)
     Button(root, text='To code', command=t2.to_code).pack()
     Button(root, text='To blocks', command=t2.to_blocks).pack()
-    t2.top.geometry("500x500")
+    t2.top.geometry("500x500+540+120")
     t2.create_bin()
     root.mainloop()
 
